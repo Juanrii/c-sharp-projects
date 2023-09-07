e858218b9acc39a913d7439e8e7d0c40763a8ac9
Juan <juanrios0495@gmail.com>
2023-09-07 09:14:40 -0300

    Skeleton 6 capas

---
+++ DAS/Skeleton6Capas/BLL/BLLClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abstraccion;
+using BE;
+using MPP;
+
+namespace BLL
+{
+    public class BLLClase : IGestor<BEClass>
+    {
+        public bool Baja(BEClass Objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Guardar(BEClass Objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BEClass ListarObjeto(BEClass Objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BEClass> ListarTodo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DAS/Skeleton6Capas/DAL/Acceso.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class Acceso
+    {
+        private SqlConnection _connection = new SqlConnection("...");
+
+        public DataSet Leer(string query)
+        {
+            DataSet dataSet = new DataSet();
+            try
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(query, _connection);
+                adapter.Fill(dataSet);
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                _connection.Close();
+            }
+            return dataSet;
+        }
+
+        public bool ExecuteNonQuery(string query)
+        {
+            try
+            {
+                _connection.Open();
+
+                SqlCommand command = new SqlCommand();
+                command.CommandType = CommandType.Text;
+                command.Connection = _connection;
+                command.CommandText = query;
+
+                int res = command.ExecuteNonQuery();
+                return true;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+
+    }
+}
