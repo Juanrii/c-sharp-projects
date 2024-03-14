7784fbaa2359f8594fd76614c0dc059bab42691d
Juanrii <55418564+Juanrii@users.noreply.github.com>
2024-03-13 22:53:43 -0300

    FastReport - .Net Integration

---
+++ DAS/ExpoReportes/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace ExpoReportes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataTable = new System.Data.DataSet();
+            this.btnPrevisualizar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnExportPDF = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataTable
+            // 
+            this.dataTable.DataSetName = "NewDataSet";
+            // 
+            // btnPrevisualizar
+            // 
+            this.btnPrevisualizar.Location = new System.Drawing.Point(39, 236);
+            this.btnPrevisualizar.Name = "btnPrevisualizar";
+            this.btnPrevisualizar.Size = new System.Drawing.Size(1187, 102);
+            this.btnPrevisualizar.TabIndex = 0;
+            this.btnPrevisualizar.Text = "Previsualizar Reporte";
+            this.btnPrevisualizar.UseVisualStyleBackColor = true;
+            this.btnPrevisualizar.Click += new System.EventHandler(this.btnPrevisualizar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 55);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1187, 159);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnExportPDF
+            // 
+            this.btnExportPDF.Location = new System.Drawing.Point(39, 516);
+            this.btnExportPDF.Name = "btnExportPDF";
+            this.btnExportPDF.Size = new System.Drawing.Size(1187, 170);
+            this.btnExportPDF.TabIndex = 2;
+            this.btnExportPDF.Text = "Exportar PDF";
+            this.btnExportPDF.UseVisualStyleBackColor = true;
+            this.btnExportPDF.Click += new System.EventHandler(this.btnExportPDF_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnGuardar);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.btnExportPDF);
+            this.groupBox1.Controls.Add(this.btnPrevisualizar);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1271, 725);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "FastReport";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(42, 376);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(1187, 102);
+            this.btnGuardar.TabIndex = 3;
+            this.btnGuardar.Text = "Guardar Cambios";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1304, 767);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "UAI - DESARROLLO Y ARQUITECTURA DE SOFTWARE";
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Data.DataSet dataTable;
+        private System.Windows.Forms.Button btnPrevisualizar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnExportPDF;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnGuardar;
+    }
+}
+
---
+++ DAS/ExpoReportes/Form1.cs
@@ -0,0 +1,132 @@
+﻿/* =============================================================================
+ * Universidad: Universidad Abierta Interamericana (UAI)
+ * Materia: DESARROLLO Y ARQUITECTURA DE SOFTWARE
+ * Tema: FastReport
+ * Año: 2024
+ * 
+ * Descripción: El objetivo es demostrar el uso de FastReport para la creación,
+ * manipulación y exportación de reportes en aplicaciones .NET, específicamente
+ * en el contexto de un proyecto de Windows Forms.
+ * =============================================================================
+ */
+
+using FastReport;
+using System;
+using System.Windows.Forms;
+
+namespace ExpoReportes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrevisualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Crea una instancia de Report
+                // Se utiliza using para luego liberar los recursos utilizados
+                using (Report report = new Report())
+                {
+                    // Carga el diseño desde un archivo .frx
+                    report.Load(@"Prueba.frx");
+
+                    // Busca un objeto de texto en el reporte por su nombre 'Text1'
+                    TextObject textObject = report.FindObject("Text1") as TextObject;
+
+                    // Si el objeto de texto existe y el textBox1 tiene algun texto,
+                    // actualiza el objeto de texto en el reporte con el contenido de textBox1
+                    if (textObject != null && textBox1.Text != String.Empty)
+                    {
+                        // Permite que el objeto de texto crezca si el texto es demasiado largo
+                        textObject.CanGrow = true;
+                        // Establece el texto del objeto
+                        textObject.Text = textBox1.Text;
+                    }
+
+                    // Muestra el reporte en el visor de FastReport
+                    report.Show();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al cargar el reporte: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Verifica si textBox1 esta vacio y muestra un mensaje de error si es asi
+                if (textBox1.Text == String.Empty)
+                {
+                    MessageBox.Show($"Debe escribir algo para guardar en el reporte.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                // Pregunta al usuario si desea guardar los cambios
+                DialogResult respuesta = MessageBox.Show($"Desea guardar los cambios?", "Guardar", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (respuesta == DialogResult.No)
+                    return;
+
+                using (Report report = new Report())
+                {
+                    report.Load(@"Prueba.frx");
+
+                    TextObject textObject = report.FindObject("Text1") as TextObject;
+                    
+                    if (textObject != null && textBox1.Text != "")
+                    {
+                        textObject.CanGrow = true;
+                        textObject.Text = textBox1.Text;
+
+                        // Guardar el reporte con los cambios realizados.
+                        report.Save(@"Prueba.frx");
+                        // Limpia el campo de texto
+                        textBox1.Text = "";
+
+                        MessageBox.Show("Reporte guardado correctamente", "Guardado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al guardar el reporte: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnExportPDF_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (Report report = new Report())
+                {
+                    report.Load(@"Prueba.frx");
+
+                    // Prepara el reporte para la exportacion
+                    report.Prepare();
+
+                    // Crea una instancia de la clase de exportacion a PDF de FastReport
+                    FastReport.Export.Pdf.PDFExport export = new FastReport.Export.Pdf.PDFExport();
+
+                    // Muestra el dialogo de configuracion de exportacion y, si el usuario confirma, exporta el reporte
+                    if (export.ShowDialog())
+                    {
+                        string archivo = "Prueba.pdf";
+                        report.Export(export, archivo);
+                        MessageBox.Show($"El reporte {archivo} fue exportado correctamente", "Exportado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al exportar el reporte: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
